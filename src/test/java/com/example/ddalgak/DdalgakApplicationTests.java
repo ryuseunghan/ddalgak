a78a00da2d1db2a0cb42cda8413c46ce2cf43a6f
ryuseunghan <jorzor99@gmail.com>
2026-03-25 21:49:15 +0900

    setting

--- src/test/java/com/example/ddalgak/DdalgakApplicationTests.java
+++ src/test/java/com/example/ddalgak/DdalgakApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.ddalgak;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class DdalgakApplicationTests {
 
 	@Test
