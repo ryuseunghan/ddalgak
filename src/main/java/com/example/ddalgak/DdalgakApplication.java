d801af3063cab1d0317ef258261d671a6653c242
ryuseunghan <jorzor99@gmail.com>
2026-03-25 20:58:41 +0900

    setting

---
+++ src/main/java/com/example/ddalgak/DdalgakApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddalgak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdalgakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdalgakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ddalgak/DdalgakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddalgak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdalgakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
